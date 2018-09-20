23e890888c93f9c0eafefdeb0787eda9842d6129
unknown <huutam.ptnk@gmail.com>
2018-09-20 15:43:28 +0300

    change order of directories

---
+++ Counter/src/components/counter.jsx
@@ -0,0 +1,16 @@
+// Type imrc for shortcut
+import React, { Component } from "react";
+
+// Type cc for shortcut
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello world</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
