8953b6153ec6c3bfd0d7acac69e78709c5197712
unknown <huutam.ptnk@gmail.com>
2018-09-20 16:14:47 +0300

    modify counter

--- Counter/src/components/counter.jsx
+++ Counter/src/components/counter.jsx
@@ -3,14 +3,39 @@ import React, { Component } from "react";
 
 // Type cc for shortcut
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+  styles = {
+    fontSize: 30,
+    fontWeigth: "bold"
+  };
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello world</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
-}
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
+  formatCount() {
+    const { count } = this.state;
+    return this.state.count === 0 ? "Zero" : count;
+  }
+}
+// <img src={this.state.imageURL} alt="" />
 export default Counter;
