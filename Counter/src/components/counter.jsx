28a81b106f31f74e47d340b3bfed1d86d64c5ffd
unknown <huutam.ptnk@gmail.com>
2018-09-20 17:06:49 +0300

    modify counter

--- Counter/src/components/counter.jsx
+++ Counter/src/components/counter.jsx
@@ -4,28 +4,32 @@ import React, { Component } from "react";
 // Type cc for shortcut
 class Counter extends Component {
   state = {
-    count: 0,
+    count: this.props.value,
     tags: ["tag1", "tag2", "tag3"]
   };
   styles = {
     fontSize: 30,
     fontWeigth: "bold"
   };
-  render() {
+  /* constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  } */
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
-      <div>
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
@@ -35,6 +39,28 @@ class Counter extends Component {
   formatCount() {
     const { count } = this.state;
     return this.state.count === 0 ? "Zero" : count;
+  }
+  render() {
+    console.log("props", this.props);
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Add new tag."}
+        {this.renderTags()}
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={
+            // if i wanna pass an argument in
+            // () => this.handleIncrement(argument)
+            this.handleIncrement
+          }
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
   }
 }
 // <img src={this.state.imageURL} alt="" />
