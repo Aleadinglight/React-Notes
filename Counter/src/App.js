ed2737ff328310fd8c48d50a279a8e07a8761683
unknown <huutam.ptnk@gmail.com>
2018-09-21 19:41:09 +0300

    modify counter

---
+++ Counter/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import "./App.css";
+import Counters from "./components/counters";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleIncrement = counter => {
+    console.log(counter);
+    // good practice
+    var counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counter });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.onDelete}
+            onIncrement={this.onIncrement}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+export default App;
