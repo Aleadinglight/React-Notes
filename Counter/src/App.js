6b830f0a4e0d440b6fc36a1dbb4860d70ff241c2
unknown <huutam.ptnk@gmail.com>
2018-09-22 19:52:53 +0300

    chang navbar

--- Counter/src/App.js
+++ Counter/src/App.js
@@ -12,6 +12,11 @@ class App extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  constructor(props) {
+    super(props);
+    console.log("App - Constructor");
+  }
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters });
--- Counter/src/components/navbar.jsx
+++ Counter/src/components/navbar.jsx
@@ -5,7 +5,7 @@ import React, { Component } from "react";
 const NavBar = props => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="index.html">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
           {props.totalCounters}
