4e239ab55376552970116096d0ee022d6783e333
unknown <huutam.ptnk@gmail.com>
2018-09-24 21:18:23 +0300

    example on how to use combineReducers

--- react_with_redux/src/index.js
+++ react_with_redux/src/index.js
@@ -3,20 +3,46 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 // The reducer read the action to update the state
-function reducer(state, action) {
+function productsReducer(state = [], action) {
   console.log(action);
-  if (action.type === "changeState") {
-    return action.payload.newState;
-  }
-  return "State";
+  return state;
 }
 
-const store = createStore(reducer);
-console.log(store.getState());
+function userReducer(state = "", { type, payload }) {
+  switch (type) {
+    case "updateUser":
+      return payload.user;
+  }
+  return state;
+}
 
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+
+const store = createStore(
+  allReducers,
+  {
+    products: [{ name: "iPhone" }],
+    user: "Micheal"
+  },
+  window.devToolsExtension && window.devToolsExtension()
+);
+
+const updateUserAction = {
+  type: "updateUser",
+  payload: {
+    user: "John"
+  }
+};
+
+store.dispatch(updateUserAction);
+
+/*
 const action = {
   type: "changeState",
   payload: {
@@ -26,6 +52,7 @@ const action = {
 // send to reducer
 store.dispatch(action);
 console.log(store.getState());
+*/
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
